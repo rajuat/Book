9fc47a8dbbf4fb544ac2206645bb1f11b92b89e8
raju.athokpam <arc783bye875#>
2016-12-27 16:32:14 +0530

    location

--- app/src/main/java/com/itservz/bookex/android/SellActivity.java
+++ app/src/main/java/com/itservz/bookex/android/SellActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
@@ -18,8 +20,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.itservz.bookex.android.model.Book;
 import com.itservz.bookex.android.preference.PrefManager;
 import com.itservz.bookex.android.service.FirebaseDatabaseService;
@@ -31,25 +37,42 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.UUID;
 
-public class SellActivity extends AppCompatActivity implements  View.OnClickListener{
-
+public class SellActivity extends AppCompatActivity implements
+        View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    protected static final String TAG = "SellActivity";
     private Book book;
     int PICK_IMAGE = 1;
     private ImageView bookImage;
     private InputStream image_stream;
     PrefManager prefManager = null;
-    private final static int  MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 1;
+    private final static int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 1;
+    protected static final int premissionCodeAccessFineLocation = 2;
+    protected static final int premissionCodeAccessCoarseLocation = 3;
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
 
+        mLatitudeLabel = "Latitude";
+        mLongitudeLabel = "Longitude";
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+
+        buildGoogleApiClient();
+
         prefManager = new PrefManager(this);
 
         book = new Book();
         bookImage = (ImageView) findViewById(R.id.book_image);
-        bookImage.setOnClickListener(new View.OnClickListener(){
+        bookImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent chooserIntent = new ImagePickerService().getImageFromPhoneIntent();
@@ -61,7 +84,7 @@ public class SellActivity extends AppCompatActivity implements  View.OnClickList
         isbn.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                prefManager.setISBN(((TextInputEditText)v).getText().toString());
+                prefManager.setISBN(((TextInputEditText) v).getText().toString());
                 //book.ISBN = ((TextInputEditText)v).getText().toString();
             }
         });
@@ -70,7 +93,7 @@ public class SellActivity extends AppCompatActivity implements  View.OnClickList
         title.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                prefManager.setTitle(((TextInputEditText)v).getText().toString());
+                prefManager.setTitle(((TextInputEditText) v).getText().toString());
                 //book.title = ((TextInputEditText)v).getText().toString();
             }
         });
@@ -108,7 +131,7 @@ public class SellActivity extends AppCompatActivity implements  View.OnClickList
             image_stream = null;
             try {
                 image_stream = getContentResolver().openInputStream(imageUri);
-                Bitmap bitmap= BitmapFactory.decodeStream(image_stream);
+                Bitmap bitmap = BitmapFactory.decodeStream(image_stream);
                 bookImage.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 if (ContextCompat.checkSelfPermission(this,
@@ -134,13 +157,97 @@ public class SellActivity extends AppCompatActivity implements  View.OnClickList
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Bitmap bitmap= BitmapFactory.decodeStream(image_stream);
+                    Bitmap bitmap = BitmapFactory.decodeStream(image_stream);
                     bookImage.setImageBitmap(bitmap);
                 } else {
                     Toast.makeText(this, "Denied", Toast.LENGTH_LONG);
                 }
                 return;
             }
+            case premissionCodeAccessFineLocation: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Permission granted for FINE_LOCATION");
+                } else {
+                    Log.d(TAG, "Permission denied for FINE_LOCATION");
+                }
+                return;
+            }
+
+            case premissionCodeAccessCoarseLocation: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Permission granted for COARSE_LOCATION");
+                } else {
+                    Log.d(TAG, "Permission denied for COARSE_LOCATION");
+                }
+                return;
+            }
         }
     }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "onConnected");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, premissionCodeAccessFineLocation);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, premissionCodeAccessFineLocation);
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.d(TAG, "Last location: " + mLastLocation);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
+                    mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
+                    mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(this, "No location detected. Make sure location is enabled on the device.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+
 }
